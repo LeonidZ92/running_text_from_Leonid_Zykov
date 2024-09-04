4bcf84894c3175716a48031665cde35347d16b0a
LeonidZ92 <60276599+LeonidZ92@users.noreply.github.com>
2024-09-04 11:29:06 +0300

    Update settings.py

--- running_text/running_text/settings.py
+++ running_text/running_text/settings.py
@@ -28,7 +28,7 @@ DEBUG = True
 ALLOWED_HOSTS = [
     'localhost',
     '127.0.0.1',
-    '.ngrok.io',  # разрешить все поддомены ngrok.io
+    '.ngrok-free.app',  # разрешить все поддомены ngrok.io
 ]
 
 
